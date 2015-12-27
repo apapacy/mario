14bc5cc9d682fc2dafc3d44946d26181e69daa3c
Андрей Овчаренко <ovcharenkoav@meta.ua>
2015-12-27 04:27:36 +0200

    example full

--- assets/js/views/admin/layoutView.js
+++ assets/js/views/admin/layoutView.js
@@ -1,16 +1,43 @@
-define(["marionette", "backbone", "twig!admin/layout", "twig!admin/leftside"]
-  , function(Marionette, Backbone, adminLayout, adminLeftside) {
+define(["marionette"
+  , "backbone"
+  , "twig!admin/layout"
+  , "twig!admin/leftside"
+  , "twig!admin/main"
+  , "twig!admin/rightside"]
+, function(Marionette
+  , Backbone
+  , adminLayout
+  , adminLeftSide
+  , adminMain
+  , adminRightSide) {
+
   var layoutView = new (Marionette.LayoutView.extend({
     template: adminLayout,
     el: "body",
   }));
+
   layoutView.init = function() {
     layoutView.render();
+
     layoutView.addRegion( "leftside", "#leftside");
     var leftSideView = new (Marionette.LayoutView.extend({
-      template: adminLeftside,
+      template: adminLeftSide,
     }));
-    layoutView.getRegion("leftside").show(leftSideView, {});
+    layoutView.getRegion("leftside").show(leftSideView);
+
+    layoutView.addRegion( "main", "#main");
+    var mainView = new (Marionette.LayoutView.extend({
+      template: adminMain,
+    }));
+    layoutView.getRegion("main").show(mainView);
+
+    layoutView.addRegion( "rightside", "#rightside");
+    var rightSideView = new (Marionette.LayoutView.extend({
+      template: adminRightSide,
+    }));
+    layoutView.getRegion("rightside").show(rightSideView);
   }
+
   return layoutView;
+
 });
